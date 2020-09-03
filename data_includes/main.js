7ab7edf191687c08b80e83a30164637c7e336afc
hyein-jeong <46813089+hyein-jeong@users.noreply.github.com>
2020-09-03 19:51:40 +0200

    Update main.js
    add consent link pop up

--- data_includes/main.js
+++ data_includes/main.js
@@ -6,6 +6,35 @@ PennController.InitiateRecorder("https://github.com/hyein-jeong/pcibex_exp2/uplo
 var showProgressBar = true;
 //var progressBarText = "Fortschritt";
 
+
+//edit text pop up for voice recording
+let replaceConsentMic = ()=>{
+        let consentLink = $(".PennController-PennController a.Message-continue-link");
+        if (consentLink.length > 0 && consentLink[0].innerHTML.match(/^By clicking this link I understand that I grant this experiment's script access to my recording device/))
+            consentLink.html("Durch klicken erteile ich diesem Skript Zugriff auf mein Mikrofon.");
+        else
+            window.requestAnimationFrame( replaceConsentMic );
+};
+
+window.requestAnimationFrame( replaceConsentMic );
+
+const replacePreloadingMessage = ()=>{
+    const preloadingMessage = $(".PennController-PennController > div");
+    if (preloadingMessage.length > 0 && preloadingMessage[0].innerHTML.match(/^<p>Please wait while the resources are preloading/))
+        preloadingMessage.html("<p>Laden, bitte warten</p>");
+    window.requestAnimationFrame( replacePreloadingMessage );
+};
+window.requestAnimationFrame( replacePreloadingMessage );
+
+const replaceUploadingMessage = ()=>{
+    const uploadingMessage = $(".PennController-PennController > p");
+    if (uploadingMessage.length > 0 && uploadingMessage[0].innerHTML.match(/^Please wait while the archive of your recordings is being uploaded to the server/))
+        uploadingMessage.html("Bitte warten Sie, bis das Archiv Ihrer Aufnahmen auf den Server hochgeladen wurde. Dies kann einige Minuten dauern.");
+    window.requestAnimationFrame( replaceUploadingMessage );
+};
+window.requestAnimationFrame( replaceUploadingMessage );
+
+
 // Show the 'intro' trial first, then all the 'experiment' trials in a random order
 // then send the results and finally show the trial labeled 'bye'
 Sequence("intro_ID",
